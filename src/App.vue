f0fdd146582e4d063b110c7d373b8724f952fd1d
David FYON <davidfyon@gmail.com>
2024-02-04 14:47:36 +0100

    clean app

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,3 @@
-<script setup lang="ts">
-import WordleBoard from "./components/WordleBoard/WordleBoard.vue";
-</script>
-
 <template>
-    <WordleBoard word-of-the-day="You did it!" />
+    Wordle
 </template>
