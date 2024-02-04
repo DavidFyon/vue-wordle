6c94687c263e6f2a75c8c6f394b8c097cf34a858
David FYON <davidfyon@gmail.com>
2024-02-04 15:20:35 +0100

    Update test message

--- src/components/WordleBoard/WordleBoard.spec.ts
+++ src/components/WordleBoard/WordleBoard.spec.ts
@@ -5,7 +5,7 @@ import WordleBoard from "./WordleBoard.vue";
 import {VICTORY_MESSAGE} from "../../../settings";
 
 describe("WordleBoard", () => {
-    test("a victory message appears when the user makes a guess that matches the word of the day", async () => {
+    test("a victory message appears", async () => {
         const wrapper = mount(WordleBoard, {props: {wordOfTheDay: "TESTS"}})
 
         expect(wrapper.text()).toContain(VICTORY_MESSAGE)
