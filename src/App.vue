2c4e947664af9ee66263319947ed87a706485387
David FYON <davidfyon@gmail.com>
2024-02-04 14:44:12 +0100

    add components + structure

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-import WordleBoard from "./components/WordleBoard/WordleBoard.vue";
+import WordleBoard from "@/components/WordleBoard/WordleBoard.vue";
 </script>
 
 <template>
-    <WordleBoard word-of-the-day="You did it!" />
+  <WordleBoard msg="You did it!"/>
 </template>
