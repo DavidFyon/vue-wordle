b19e8be0fb25744f87c242425000d01b79307105
David FYON <davidfyon@gmail.com>
2024-02-28 11:22:46 +0100

    extract guees input into a component

--- src/components/WordleBoard/WordleBoard.vue
+++ src/components/WordleBoard/WordleBoard.vue
@@ -1,50 +1,50 @@
-<script setup lang="ts">
-import {VICTORY_MESSAGE, DEFEAT_MESSAGE, WORD_SIZE} from "../../../settings";
-import fiveLettersWords from "../../../wordsWith5Letters.json"
-import {computed, ref} from "vue";
+<script lang="ts" setup>
+import {DEFEAT_MESSAGE, VICTORY_MESSAGE} from "../../../settings";
+import wordsWith5Letters from "../../../wordsWith5Letters.json";
+import {ref} from "vue";
+import GuessInput from "../../components/GuessInput/GuessInput.vue";
 
 defineProps({
   wordOfTheDay: {
     type: String,
-    validator: (wordGiven: string) => fiveLettersWords.includes(wordGiven)
+    validator: (wordGiven: string) => wordsWith5Letters.includes(wordGiven)
   }
 })
 
-const guessInProgress = ref<string | null>(null)
 const guessSubmitted = ref("")
-
-const formattedGuessInProgress = computed<string>({
-  get() {
-    return guessInProgress.value ?? ""
-  },
-  set(rawValue: string) {
-    guessInProgress.value = null
-
-    guessInProgress.value = rawValue
-        .slice(0, WORD_SIZE)
-        .toUpperCase()
-        .replace(/[^A-Z]+/gi, "")
-  }
-})
-
-function onSubmit() {
-  if (!fiveLettersWords.includes(formattedGuessInProgress.value)) {
-    return
-  }
-
-  guessSubmitted.value = formattedGuessInProgress.value
-}
 </script>
 
 <template>
-  <input
-      v-model="formattedGuessInProgress"
-      :maxlength="WORD_SIZE"
-      type="text"
-      @keydown.enter="onSubmit"
-  >
-  <p
-      v-if="guessSubmitted.length > 0"
-      v-text="guessSubmitted === wordOfTheDay ? VICTORY_MESSAGE : DEFEAT_MESSAGE"
-  />
+  <main>
+    <guess-input @guess-submitted="guess => guessSubmitted = guess"/>
+
+    <p v-if="guessSubmitted.length > 0"
+       class="end-of-game-message"
+       v-text="guessSubmitted === wordOfTheDay ? VICTORY_MESSAGE : DEFEAT_MESSAGE"/>
+  </main>
 </template>
+
+<style scoped>
+main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 3rem;
+}
+.end-of-game-message {
+  font-size: 3rem;
+  animation: end-of-game-message-animation 700ms forwards;
+  white-space: nowrap;
+  text-align: center;
+}
+@keyframes end-of-game-message-animation {
+  0% {
+    opacity: 0;
+    transform: rotateZ(0);
+  }
+  100% {
+    opacity: 1;
+    transform: translateY(2rem);
+  }
+}
+</style>
